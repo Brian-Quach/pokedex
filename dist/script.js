4d5551d862556e9bc5581715f47913b71e62d74f
Brian Quach <briiquach@gmail.com>
2020-07-22 00:43:49 -0400

    Pokedex done for now

--- dist/script.js
+++ dist/script.js
@@ -1,46 +1,55 @@
 var pokemonId = 25;
 
-window.addEventListener('DOMContentLoaded', (event) => {
-	document.getElementById("pokemon-number").addEventListener("keyup", (e) =>{
-    event.preventDefault();
-		
+window.addEventListener("DOMContentLoaded", (event) => {
+	document.getElementById("pokemon-number").addEventListener("keyup", (e) => {
+		event.preventDefault();
+
 		if (e.keyCode === 13) {
-			searchPokemon(e.target.value)
+			pokemonId = e.target.value;
+			searchPokemon(e.target.value);
 		}
 	});
-	
-	searchPokemon(pokemonId)
-	
+
+	searchPokemon(pokemonId);
+
 	document.getElementById("nav-button-left").addEventListener("click", (_) => {
-		if(pokemonId != 1) {
+		if (pokemonId != 1) {
 			pokemonId--;
 			searchPokemon(pokemonId);
 		}
-	})
+	});
 	document.getElementById("nav-button-right").addEventListener("click", (_) => {
-		if(pokemonId != 807) {
+		if (pokemonId != 807) {
 			pokemonId++;
 			searchPokemon(pokemonId);
 		}
-	})
+	});
 });
 
-function setupUi() {
-	
-}
+function setupUi() {}
 
 async function searchPokemon(id) {
-	console.log(id)
+	console.log(id);
 	let pokemon = await getPokemonData(id);
-	console.log(pokemon)
-	document.getElementById('pokemon-image').setAttribute("src", pokemon.sprites.front_default);
-	document.getElementById('pokemon-name').innerHTML = pokemon.name;
-	document.getElementById('pokemon-type').innerHTML = `Type ${pokemon.types[0].type.name}` //TODO Fix
-	document.getElementById('pokemon-height').innerHTML = `Height ${(pokemon.height*0.1).toFixed(1)}m`
-	document.getElementById('pokemon-weight').innerHTML = `Weight ${(pokemon.weight*0.1).toFixed(1)}kg`
-	
-	
-	document.getElementById('pokemon-number').value = id;
+	let pokemonTypes = pokemon.types.map(getTypeSpan);
+	console.log(pokemonTypes);
+	console.log(pokemon);
+	document
+		.getElementById("pokemon-image")
+		.setAttribute("src", pokemon.sprites.front_default);
+	document.getElementById("pokemon-name").innerHTML = pokemon.name;
+	document.getElementById("pokemon-type").innerHTML = `Type: ${pokemonTypes}`; //TODO Fix
+	document.getElementById("pokemon-height").innerHTML = `Height: ${(
+		pokemon.height * 0.1
+	).toFixed(1)}m`;
+	document.getElementById("pokemon-weight").innerHTML = `Weight: ${(
+		pokemon.weight * 0.1
+	).toFixed(1)}kg`;
+	document.getElementById("pokemon-number").value = id;
+}
+
+function getTypeSpan(typeData) {
+	return `<span class="type type-${typeData.type.name}">${typeData.type.name}</span>`;
 }
 
 async function getPokemonData(id) {
